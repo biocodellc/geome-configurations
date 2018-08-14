b41bd5bb5760a864344e2722610dfea92bd08dc6
John Deck <jdeck88@gmail.com>
2018-08-14 04:23:35 -0700

    first files, porting from biocode-exports

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # geome-configurations
 Manage project configurations for geome
+
+
+```
+cd bin
+curl https://api.develop.geome-db.org/projects/1/config > template.json.gz
+gunzip template.json.gz
+# run the following script to loop through projects and load
+./put.sh
+```
---
+++ bin/put.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+ 
+#Script to generate configurations for each project
+
+echo "Putting JSON to server...."
+
+# Biocode Family of Projects
+for projectID in 1 3 4 5 6 7 8 9 10 11 13
+
+# Dipnet project
+#for projectID in 2
+
+do
+    curl -X PUT -H 'Content-Type: application/json' --data "@/home/jdeck/code/biocode-exports/scripts/createBiocodeTemplateProjects/template.json" https://api.develop.geome-db.org/projects/$projectID/config?access_token=RkG54qJjRCHvawngVNqm
+done
