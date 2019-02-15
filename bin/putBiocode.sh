1b1a2c41892dff94db0640798b92c6938352f115
John Deck <jdeck88@gmail.com>
2019-02-14 14:57:11 -1000

    improving the put biocode code script

--- bin/putBiocode.sh
+++ bin/putBiocode.sh
@@ -4,33 +4,29 @@
 
 usage()
 {
-    echo "usage: putBiocode.sh [DEV|PROD] [access_token]"
+    echo "usage: putBiocode.sh [DEV|PROD] [access_token] [projectID] [file_path]"
 }
 
 run() 
 {
     echo "Putting JSON to server...."
     # Set server
-    if [ "$1" = "DEV" ]; then
-	# PEER
-        projectID=7
+    if [ "$server" = "DEV" ]; then
         url=https://api.develop.geome-db.org/projects/$projectID/config?access_token=$access_token
     else
-	# PEER
-        projectID=2
         url=https://api.geome-db.org/projects/$projectID/config?access_token=$access_token
     fi
     echo "endpoint: "$url
     curl -X PUT -H 'Content-Type: application/json' --data "@$file_path" $url
 }
 
-# Set File path
-file_path=/home/jdeck/code/geome-configurations/bin/biocode.json
-
 # Set access_toekn
+server=$1
 access_token=$2
+projectID=$3
+file_path=$4
 
-if [ $# -ne 2 ]
+if [ $# -ne 4 ]
 then
     usage
     exit
